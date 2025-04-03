8e56d99390a99d3be0e01aa4cdd083a329fd7631
David Alexander <opensource@thelonelyghost.com>
2025-04-03 17:07:42 -0400

    Backfills neovim configs

---
+++ configs/nvim/lua/plugins/base.lua
@@ -0,0 +1,31 @@
+return {
+	-- add gruvbox
+	{ "ellisonleao/gruvbox.nvim" },
+
+	-- Configure LazyVim to load gruvbox
+	{
+		"LazyVim/LazyVim",
+		opts = {
+			colorscheme = "gruvbox",
+		},
+	},
+
+	{
+		"mfusseneger/nvim-lint",
+		opts = {
+			linters_by_ft = {
+				sh = { "shellcheck" },
+			},
+		},
+	},
+
+	-- add cmp-emoji
+	{
+		"hrsh7th/nvim-cmp",
+		dependencies = { "hrsh7th/cmp-emoji" },
+		---@param opts cmp.ConfigSchema
+		opts = function(_, opts)
+			table.insert(opts.sources, { name = "emoji" })
+		end,
+	},
+}
