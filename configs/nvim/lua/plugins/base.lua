c2c286d278dd2bfa9b68213f59dd2f41f7262ced
David Alexander <opensource@thelonelyghost.com>
2025-04-09 15:27:14 -0400

    Sets keybindings for Trouble diagnostics

--- configs/nvim/lua/plugins/base.lua
+++ configs/nvim/lua/plugins/base.lua
@@ -28,4 +28,22 @@ return {
 			table.insert(opts.sources, { name = "emoji" })
 		end,
 	},
+
+	{
+		"folke/trouble.nvim",
+		opts = {},
+		cmd = "Trouble",
+		keys = {
+			{
+				"<leader>xx",
+				"<cmd>Trouble diagnostics toggle<cr>",
+				desc = "Diagnostics (Trouble)",
+			},
+			{
+				"<leader>xX",
+				"<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+				desc = "Buffer Diagnostics (Trouble)",
+			},
+		},
+	},
 }
